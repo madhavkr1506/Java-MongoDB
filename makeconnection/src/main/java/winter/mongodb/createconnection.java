9020575680a9be71fdbc3e652319b0f7c20cb1df
Madhav Kumar <117391373+madhavkr1506@users.noreply.github.com>
2025-01-12 13:08:41 +0530

    Add files via upload

--- makeconnection/src/main/java/winter/mongodb/createconnection.java
+++ makeconnection/src/main/java/winter/mongodb/createconnection.java
@@ -1,68 +1,72 @@
 package winter.mongodb;
 
 import java.util.Arrays;
-
-// import java.util.Arrays;
-// import java.util.List;
+import java.util.List;
 
 import org.bson.Document;
+// import org.bson.types.ObjectId;
 
+import com.mongodb.MongoException;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+// import com.mongodb.client.model.Projections;
+import com.mongodb.client.model.Sorts;
 
 public class createconnection {
     public static void main(String[] args) {
-        String uri = "mongodb://localhost:27017";
-        try(MongoClient client = MongoClients.create(uri)){
-            MongoDatabase database = client.getDatabase("students");
-            System.out.println("Connected to the database: " + database.getName());
+        try(MongoClient client = MongoClients.create("mongodb://localhost:27017")){
 
-            // lets create collection
-            MongoCollection<Document> collection = database.getCollection("class");
-            // if(collection == null){
-            //     database.createCollection("class");
-            //     System.out.println("Collection created successfully");
-            // }else{
-            //     System.out.println("Collection already exists");
+            MongoDatabase database = client.getDatabase("shop");
+            // System.out.println("Database Name: " + database.getName());
+            MongoCollection<Document> collection = database.getCollection("products");
+            // if(collection != null){
+                // System.out.println("Collection Found");
             // }
 
-            // Document document1 = new Document("name","Shreya").append("marks", 80).append("age",21).append("location", "patna");
-        //     List<Document> documents = Arrays.asList(new Document("name", "Khushi").append("marks", 80).append("age", 21).append("location", "Patna"),
-        //     new Document("name", "Rahul").append("marks", 90).append("age", 22).append("location", "Delhi"),
-        //     new Document("name", "Ananya").append("marks", 85).append("age", 20).append("location", "Mumbai"),
-        //     new Document("name", "Amit").append("marks", 76).append("age", 23).append("location", "Kolkata"),
-        //     new Document("name", "Neha").append("marks", 88).append("age", 22).append("location", "Chennai"),
-        //     new Document("name", "Ravi").append("marks", 70).append("age", 21).append("location", "Bangalore"),
-        //     new Document("name", "Priya").append("marks", 92).append("age", 24).append("location", "Hyderabad"),
-        //     new Document("name", "Saurabh").append("marks", 78).append("age", 22).append("location", "Pune"),
-        //     new Document("name", "Maya").append("marks", 95).append("age", 23).append("location", "Lucknow"),
-        //     new Document("name", "Vikas").append("marks", 83).append("age", 21).append("location", "Varanasi")
-        // );
-        //     collection.insertMany(documents);
 
-        // System.out.println("Document inserted successfully");
+            // write query to find documents where price is 799
+            // Document document1 = new Document("price",799);
+            // collection.find(document1).forEach(document -> System.out.println(document.toJson()));
 
-            // Document find_document = new Document("name","Shreya");
-            // collection.find(find_document).forEach(document -> System.out.println(document.toJson()));
+            // Retrieval: Write a query to find all documents where isFeatured is true.
+            // Document document2 = new Document("isFeatured",true);
+            // collection.find(document2).forEach(document -> System.out.println(document.toJson()));
 
-            // Document find_marks = new Document("marks",1);
-            // Document result = collection.find(find_document).projection(find_marks).first();
-            // if(result != null){
-            //     System.out.println("Marks: " + result.get("marks"));
-            // }else{
-            //     System.out.println("No document found for shreya");
-            // }
+            
+            // Projection: How can you retrieve only the name and price fields for all documents?
+            // collection.find().projection(Projections.include("name","price")).limit(5).forEach(document -> System.out.println(document.toJson()));
 
-            // Document find_gt = new Document("age",new Document("$gt",21 ));
-            // collection.find(find_gt).forEach(document -> System.out.println(document));
+            // Filtering: Write a query to find all products priced above 500.
 
-            // Document find_in = new Document("name",new Document("$in",Arrays.asList("Madhav","Shreya")));
-            Document find_and = new Document("$and",Arrays.asList(new Document("name","Madhav"),new Document("location","patna")));
-            collection.find(find_and).forEach(document ->System.out.println(document));
+            // collection.find(new Document("price",new Document("$gt",500))).limit(5).forEach(document -> System.out.println(document.toJson()));
 
-        }catch(Exception e){
+            // Updating: How can you update the price of the product with _id: 64c23601e32f4a51b19b926d to 850?
+            // Document document3 = new Document("_id" , new ObjectId("64c23601e32f4a51b19b926d"));
+            // collection.updateOne(document3,new Document("$set",new Document("price",850)));
+            // collection.find(document3).forEach(document -> System.out.println(document.toJson()));
+
+            // Array queue: Write a query to find all products with a specific color(e.g., "red") in colors array.
+            
+            // collection.find().limit(5).forEach(document -> System.out.println(document.toJson()));
+            // collection.find(new Document("colors","#333333")).limit(5).forEach(document -> System.out.println(document.toJson()));
+
+            // Sorting : How you can sort all products in ascending order of their price?
+            // collection.find().sort(Sorts.ascending("price")).forEach(document -> System.out.println(document.toJson()));
+
+            // Aggregation: write an aggregation pipeline to calculate the average price of products in a specific category.
+
+            // List<Document> document4 = Arrays.asList(new Document("$group", new Document("_id","$category").append("Average_price", new Document("$avg","$price"))));
+            // collection.aggregate(document4).forEach(document -> System.out.println(document.toJson()));
+
+
+            
+
+
+            
+
+        }catch(MongoException e){
             e.printStackTrace();
         }
     }
